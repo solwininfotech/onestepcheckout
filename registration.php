7d95e7531a08f22cdc3c66a1124e4c7984cb11ce
SolwinInfotech <stdabhoya@yahoo.com>
2020-11-19 13:52:41 +0530

    initial release

---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Solwin Infotech
+ * Solwin OneStepCheckout Extension
+ *
+ * @category   Solwin
+ * @package    Solwin_OneStepCheckout
+ * @copyright  Copyright © 2006-2020 Solwin (https://www.solwininfotech.com)
+ * @license    https://www.solwininfotech.com/magento-extension-license/
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Solwin_OneStepCheckout',
+    __DIR__
+);
